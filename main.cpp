5448f9a8520b428c3a8c1059ab6f9643fc1ad02e
Viktor Tsvigo <11_70@bk.ru>
2024-07-02 00:10:07 +0300

    Описание коммита

--- main.cpp
+++ main.cpp
@@ -24,6 +24,9 @@ int var;
 int neuron_index = 0, synapse_index = 0;
 std::vector<mpz_class> list_of_neurons;
 std::vector<mpz_class> list_of_synapses;
+constexpr size_t NUM_SYNAPSES = 10105;
+//const mpz_class MAX_VALUE("18446744073709551615");
+const std::string FILE_PATH = "/home/viktor/my_projects_qt_2/sgenerirovaty_sinapsi/random_sinapsi.bin";
 //##################################################################
 //######################## 5 ##################################################
 // Функция для чтения 205 long long чисел из бинарного файла
@@ -46,37 +49,78 @@ std::vector<mpz_class> read205LongLongFromBinaryFile(const std::string &filename
 //###########################################################################
 //############################# 8 ##############################################
 // Функция для чтения mpz_class из QDataStream
-void readMpzFromStream(QDataStream &in, mpz_class &number) {
-    QByteArray byteArray;
-    in >> byteArray;
+// void readMpzFromStream(QDataStream &in, mpz_class &number) {
+//     QByteArray byteArray;
+//     in >> byteArray;
 
-    // Инициализация mpz_class из массива байтов
-    number.set_str(byteArray.toStdString(), 10);
-}
+//     // Инициализация mpz_class из массива байтов
+//     number.set_str(byteArray.toStdString(), 10);
+// }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Функция для чтения чисел из бинарного файла
-std::vector<mpz_class> readNumbersFromFile2(const QString &fileName, size_t count)
-{
-    std::vector<mpz_class> list_of_synapses;
-    list_of_synapses.reserve(count);
+// Функция для чтения чисел-синапсов из бинарного файла
+// std::vector<mpz_class> readNumbersFromFile2(const QString &fileName, size_t count)
+// {
+//     std::vector<mpz_class> list_of_synapses;
+//     list_of_synapses.reserve(count);
 
-    QFile file(fileName);
-    if (!file.open(QIODevice::ReadOnly)) {
-        std::cerr << "Failed to open file for reading." << std::endl;
-        return list_of_synapses;
+//     QFile file(fileName);
+//     if (!file.open(QIODevice::ReadOnly)) {
+//         std::cerr << "Failed to open file for reading." << std::endl;
+//         return list_of_synapses;
+//     }
+
+//     QDataStream in(&file);
+//     mpz_class number;
+//     while (list_of_synapses.size() < count && !in.atEnd()) {
+//         readMpzFromStream(in, number);
+//         list_of_synapses.push_back(number);
+//     }
+
+//     file.close();
+//     return list_of_synapses;
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// std::vector<mpz_class> read_sinapsi_FromBinaryFile(const std::string &filename)
+// {
+//     std::ifstream file(filename, std::ios::binary);
+//     if (!file) {
+//         throw std::runtime_error("Ошибка открытия бинарного файла.");
+//     }
+
+//     std::vector<mpz_class> list_of_synapses(10105);
+//     file.read(reinterpret_cast<char *>(list_of_synapses.data()), 10105 * sizeof(mpz_class));
+
+//     // if (file.gcount() != 10105 * sizeof(mpz_class))
+//     // {
+//     //     throw std::runtime_error("Недостаточно данных в бинарном файле.");
+//     // }
+
+//     return list_of_synapses;
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void readFromFile(std::vector<mpz_class>& list_of_synapses, const std::string& filePath) {
+    std::ifstream inFile(filePath, std::ios::binary);
+    if (!inFile) {
+        qCritical() << "Ошибка открытия файла для чтения";
+        return;
     }
 
-    QDataStream in(&file);
-    mpz_class number;
-    while (list_of_synapses.size() < count && !in.atEnd()) {
-        readMpzFromStream(in, number);
-        list_of_synapses.push_back(number);
+    for (size_t i = 0; i < NUM_SYNAPSES; ++i) {
+        size_t size;
+        inFile.read(reinterpret_cast<char*>(&size), sizeof(size));
+        std::vector<char> buffer(size);
+        inFile.read(buffer.data(), size);
+        list_of_synapses[i].set_str(std::string(buffer.begin(), buffer.end()), 10);
     }
 
-    file.close();
-    return list_of_synapses;
+    inFile.close();
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void printVector(const std::vector<mpz_class>& list_of_synapses) {
+    for (const auto& value : list_of_synapses) {
+        qDebug() << QString::fromStdString(value.get_str());
+    }
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Пример использования функции
@@ -115,6 +159,9 @@ mpz_class activationFunction( // long long list_of_neurons.at(var)
     return (  list_of_neurons.at(var));
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// конец объявлений функций
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
@@ -122,5 +169,50 @@ int main(int argc, char *argv[])
 //########################################################################################################
     std::cout << "Funktsiya_Resheniya_6" << std::endl;
 //########################################################################################################
+// читаем синапсы из файла в вектор
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*
+    const QString fileName
+        =
+        "/home/viktor/my_projects_qt_2/sgenerirovaty_sinapsi/random_sinapsi.bin"
+        ; // Имя бинарного файла*/
+    // Преобразование QString в std::string
+    std::string stdFileName_sinapsi = fileName.toStdString();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    const size_t numberCount = 10105;
+  //  list_of_synapses= read_sinapsi_FromBinaryFile(stdFileName_sinapsi) ; //readNumbersFromFile2(fileName, numberCount) ;
+    // Чтение чисел из бинарного файла
+
+    std::vector<mpz_class> read_synapses(NUM_SYNAPSES);
+  readFromFile(read_synapses, FILE_PATH);
+    // Проверка, что прочитано правильное количество чисел
+    // if (list_of_synapses.size() != numberCount) {
+    //     std::cerr << "File does not contain the expected number of numbers." << std::endl;
+    // }
+    std::cout << "list_of_synapses.size() =" << list_of_synapses.size() << std::endl;
+    std::cout << "конец чтения синапсов в вектор" << std::endl;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Проверка содержимого вектора
+
+    // Проверка прочитанных чисел
+    // for (size_t i = 0; i < NUM_SYNAPSES; ++i) {
+    //     if (list_of_synapses[i] != read_synapses[i]) {
+    //         qCritical() << "Ошибка: значения не совпадают на позиции" << i;
+    //         return -1;
+    //     }
+    // }
+
+    // qDebug() << "Все значения совпадают.";
+
+    // Вывод значений вектора в консоль
+    printVector(read_synapses);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//"
+                 "#################################################################################"
+                 "#######################"
+              << std::endl;
+    //###########################################################################
+    //////////////////// считали синапсы в вектор //////////////////////////////////////////////////////////////////////
+
+
     return a.exec();
 }
